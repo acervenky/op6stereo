79cd34dbf9eb670847f03118eeeb5b697871f054
acervenky <acervenky@gmail.com>
2018-12-05 13:22:44 +0530

    Update .aml.sh

--- common/.aml.sh
+++ common/.aml.sh
@@ -12,36 +12,20 @@ patch_mixer_toplevel() {
   esac
 }
 
-patch_mixer_toplevel -c "EAR PA Gain" "G_8_DB"
-patch_mixer_toplevel -c "RX0 Digital Volume" "90"
-patch_mixer_toplevel -c "RX7 Digital Volume" "88"
 patch_mixer_toplevel -c "SLIM RX0 MUX" "AIF1_PB"
 patch_mixer_toplevel -c "RX INT0_1 MIX1 INP0" "RX0"
-patch_mixer_toplevel -c "RX HPH Mode" "CLS_H_HIFI"
-patch_mixer_toplevel -p "deep-buffer-playback quat_i2s" "QUAT_MI2S_RX Audio Mixer MultiMedia1" "SLIMBUS_0_RX Audio Mixer MultiMedia1" "1"
-patch_mixer_toplevel -p "low-latency-playback quat_i2s" "QUAT_MI2S_RX Audio Mixer MultiMedia5" "SLIMBUS_0_RX Audio Mixer MultiMedia5" "1"
-patch_mixer_toplevel -p "compress-offload-playback quat_i2s" "QUAT_MI2S_RX Audio Mixer MultiMedia4" "SLIMBUS_0_RX Audio Mixer MultiMedia4" "1"
-sed -i "/ *<path name=\"speaker\">/a\      <ctl name=\"SLIMBUS_0_RX Audio Mixer MultiMedia1\" value=\"1\" \/>\n\
-      <ctl name=\"QUAT_MI2S_RX Channels\" value=\"One\" \/>\n\
-      <ctl name=\"SLIM RX0 MUX\" value=\"AIF1_PB\" \/>\n\
-      <ctl name=\"CDC_IF RX0 MUX\" value=\"SLIM RX0\" \/>\n\
-      <ctl name=\"SLIM_0_RX Channels\" value=\"One\" \/>\n\
-      <ctl name=\"RX INT0_1 MIX1 INP0\" value=\"RX0\" \/>\n\
-      <ctl name=\"RX INT0 DEM MUX\" value=\"CLSH_DSM_OUT\" \/>\n\
-      <ctl name=\"EAR SPKR PA Gain\" value=\"G_6_DB\" \/>\n\
+sed -i "/ *<path name=\"deep-buffer-playback quat_i2s\">/a\      <ctl name=\"SLIMBUS_0_RX Audio Mixer MultiMedia1\" value=\"1\" \/>\n\
       <ctl name=\"RX0 HPF cut off\" value=\"MIN_3DB_150Hz\" \/>\n\
-      <ctl name=\"RX0 Digital Volume\" value=\"90\" \/>\n\
-      <ctl name=\"RX7 Digital Volume\" value=\"88\" \/>\n\
+      <ctl name=\"RX0 Digital Volume\" value=\"84\" \/>\n\
+      <ctl name=\"RX7 Digital Volume\" value=\"84\" \/>\n\
       <ctl name=\"SLIM_0_RX SampleRate\" value=\"KHZ_96\" \/>" $MODPATH/system/vendor/etc/mixer_paths_tavil.xml
-sed -i "/ *<path name=\"speaker-mono\">/a\      <ctl name=\"SLIMBUS_0_RX Audio Mixer MultiMedia1\" value=\"1\" \/>\n\
-      <ctl name=\"QUAT_MI2S_RX Channels\" value=\"One\" \/>\n\
-      <ctl name=\"SLIM RX0 MUX\" value=\"AIF1_PB\" \/>\n\
-      <ctl name=\"CDC_IF RX0 MUX\" value=\"SLIM RX0\" \/>\n\
-      <ctl name=\"SLIM_0_RX Channels\" value=\"One\" \/>\n\
-      <ctl name=\"RX INT0_1 MIX1 INP0\" value=\"RX0\" \/>\n\
-      <ctl name=\"RX INT0 DEM MUX\" value=\"CLSH_DSM_OUT\" \/>\n\
-      <ctl name=\"EAR SPKR PA Gain\" value=\"G_6_DB\" \/>\n\
+sed -i "/ *<path name=\"low-latency-playback quat_i2s\">/a\      <ctl name=\"SLIMBUS_0_RX Audio Mixer MultiMedia5\" value=\"1\" \/>\n\
       <ctl name=\"RX0 HPF cut off\" value=\"MIN_3DB_150Hz\" \/>\n\
-      <ctl name=\"RX0 Digital Volume\" value=\"90\" \/>\n\
-      <ctl name=\"RX7 Digital Volume\" value=\"88\" \/>\n\
+      <ctl name=\"RX0 Digital Volume\" value=\"84\" \/>\n\
+      <ctl name=\"RX7 Digital Volume\" value=\"84\" \/>\n\
+      <ctl name=\"SLIM_0_RX SampleRate\" value=\"KHZ_96\" \/>" $MODPATH/system/vendor/etc/mixer_paths_tavil.xml
+sed -i "/ *<path name=\"compress-offload-playback quat_i2s\">/a\      <ctl name=\"SLIMBUS_0_RX Audio Mixer MultiMedia4\" value=\"1\" \/>\n\
+      <ctl name=\"RX0 HPF cut off\" value=\"MIN_3DB_150Hz\" \/>\n\
+      <ctl name=\"RX0 Digital Volume\" value=\"84\" \/>\n\
+      <ctl name=\"RX7 Digital Volume\" value=\"84\" \/>\n\
       <ctl name=\"SLIM_0_RX SampleRate\" value=\"KHZ_96\" \/>" $MODPATH/system/vendor/etc/mixer_paths_tavil.xml
